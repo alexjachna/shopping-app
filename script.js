c12cebb25dc9627798b14673a3ba16f96a5ddecd
alexjachna <57778785+alexjachna@users.noreply.github.com>
2023-03-30 22:16:06 -0400

    Added files containing products and shopping-cart
    Files contain a basic version of products-page and shopping-cart

---
+++ script.js
@@ -0,0 +1,7 @@
+function showCart() {
+    document.getElementById('shopping-cart').style.display = "flex";
+}
+
+function hideCart() {
+    document.getElementById('shopping-cart').style.display = "none";
+}
